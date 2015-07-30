a98ea7dc72f96e9a740649dbc8c8d279ed332fd4
mmangiamusardo <m.mangiamusardo@gmail.com>
2015-07-30 19:35:59 +0200

    first commit

---
+++ WebShopCase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebShopCase.MvcApplication" Language="C#" %>
---
+++ WebShopCase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShopCase.Startup))]
+namespace WebShopCase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
