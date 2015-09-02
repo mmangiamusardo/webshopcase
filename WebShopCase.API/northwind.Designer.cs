ac9fde686ace8865439621df87dfbb3b498b5eb4
marcomangia <m.mangiamusardo@gmail.com>
2015-09-02 12:59:20 +0200

    missing files

--- WebShopCase.API/northwind.Designer.cs
+++ WebShopCase.API/northwind.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Programming\PROJECTS\JOB\TryCatch\WebShopCase\WebShopCase.API\northwind.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\JOBS\trycatchus\webshopcase\WebShopCase\WebShopCase.API\northwind.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
